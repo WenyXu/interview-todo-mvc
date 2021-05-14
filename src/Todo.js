73deb7ffc52b9fe1756cad34b600592d46a7c3dc
hmalik88 <hbmalik88@gmail.com>
2021-05-14 04:40:30 -0400

    added svg-loader to webpack, added in icons for completed/non-completed todo status, added handler functions to toggle between the two icons, added some css for margin and size.

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ListGroupItem } from 'reactstrap';
+import circle from './assets/circle-regular.svg'
+import checkCircle from './assets/check-circle-regular.svg';
 
 function Todo({text}) {
+    const [isCompleted, setComplete] = useState(false);
+
+    const handleClick = e => {
+        setComplete(!isCompleted);
+        if (isCompleted) e.target.src = circle;
+        else e.target.src = checkCircle;
+    }
 
     return(
-        <ListGroupItem>{text}</ListGroupItem>
+        <ListGroupItem>
+            <img className="circle" 
+                 src={circle} 
+                 alt="" 
+                 onClick={handleClick}
+                 />
+            {text}
+        </ListGroupItem>
     )
 }
 
